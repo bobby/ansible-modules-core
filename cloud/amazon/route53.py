214e3e34fc4f052239fa44756864c3b3a4ff663d
Bobby Calderwood <bobby@b23.io>
2015-01-21 13:23:01 -0500

    Support creation/deletion of Hosted Zones

--- cloud/amazon/route53.py
+++ cloud/amazon/route53.py
@@ -18,9 +18,9 @@ DOCUMENTATION = '''
 ---
 module: route53
 version_added: "1.3"
-short_description: add or delete entries in Amazons Route53 DNS service
+short_description: add or delete zones and records in Amazons Route53 DNS service
 description:
-     - Creates and deletes DNS records in Amazons Route53 service
+     - Creates and deletes DNS zones and records in Amazons Route53 service
 options:
   state:
     description:
@@ -31,14 +31,26 @@ options:
     choices: [ 'list', 'present', 'absent' ]
   zone:
     description:
-      - The DNS zone to modify
+      - The DNS zone to create, modify, or delete
     required: true
     default: null
     aliases: []
+  vpc_id:
+    description:
+      - The VPC to associate with this hosted zone.  If this option is provided, the hosted zone will be private.
+    required: false
+    default: null
+    aliases: []
+  region:
+    description:
+      - The AWS region for the VPC to associate with this hosted zone.  Must be specified if vpc_id is provided.
+    required: false
+    default: null
+    aliases: [aws_region, ec2_region]
   record:
     description:
-      - The full DNS record to create or delete
-    required: true
+      - The full DNS record to create or delete.
+    required: false
     default: null
     aliases: []
   ttl:
@@ -49,8 +61,8 @@ options:
     aliases: []
   type:
     description:
-      - The type of DNS record to create
-    required: true
+      - The type of DNS record to create.  Must be specified if record is provided.
+    required: false
     default: null
     aliases: []
     choices: [ 'A', 'CNAME', 'MX', 'AAAA', 'TXT', 'PTR', 'SRV', 'SPF', 'NS' ]
@@ -89,6 +101,23 @@ author: Bruce Pennypacker
 '''
 
 EXAMPLES = '''
+# Add new.foo.com zone (public hosted zone)
+- route53:
+      state: present
+      zone: foo.com
+
+# Add new.foo.com zone in a VPC (private hosted zone)
+- route53:
+      state: present
+      zone: foo.com
+      vpc_id: vpc-1234abcd
+      region: us-east-1
+
+# Delete new.foo.com zone (public hosted zone)
+- route53:
+      state: absent
+      zone: foo.com
+
 # Add new.foo.com as an A record with 3 IPs
 - route53:
       state: present
@@ -165,9 +194,10 @@ def main():
     argument_spec.update(dict(
             state           = dict(choices=['list', 'present', 'absent'], required=True),
             zone            = dict(required=True),
-            record          = dict(required=True),
+            vpc_id          = dict(required=False),
+            record          = dict(required=False),
             ttl             = dict(required=False, default=3600),
-            type            = dict(choices=['A', 'CNAME', 'MX', 'AAAA', 'TXT', 'PTR', 'SRV', 'SPF', 'NS'], required=True),
+            type            = dict(choices=['A', 'CNAME', 'MX', 'AAAA', 'TXT', 'PTR', 'SRV', 'SPF', 'NS'], required=False),
             value           = dict(required=False),
             overwrite       = dict(required=False, type='bool'),
             retry_interval  = dict(required=False, default=500)
@@ -175,33 +205,19 @@ def main():
     )
     module = AnsibleModule(argument_spec=argument_spec)
 
-    state_in              = module.params.get('state')
-    zone_in               = module.params.get('zone')
-    ttl_in                = module.params.get('ttl')
-    record_in             = module.params.get('record')
-    type_in               = module.params.get('type')
-    value_in              = module.params.get('value')
-    retry_interval_in     = module.params.get('retry_interval')
+    state_in          = module.params.get('state')
+    zone_in           = module.params.get('zone')
+    ttl_in            = module.params.get('ttl')
+    record_in         = module.params.get('record')
+    type_in           = module.params.get('type')
+    value_in          = module.params.get('value')
+    retry_interval_in = module.params.get('retry_interval')
+    vpc_in            = module.params.get('vpc_id')
 
     ec2_url, aws_access_key, aws_secret_key, region = get_ec2_creds(module)
 
-    value_list = ()
-
-    if type(value_in) is str:
-        if value_in:
-            value_list = sorted(value_in.split(','))
-    elif type(value_in)  is list:
-        value_list = sorted(value_in)
-
     if zone_in[-1:] != '.':
         zone_in += "."
-
-    if record_in[-1:] != '.':
-        record_in += "."
-
-    if state_in == 'present' or state_in == 'absent':
-        if not value_in:
-            module.fail_json(msg = "parameter 'value' required for present/absent")
 
     # connect to the route53 endpoint
     try:
@@ -216,61 +232,101 @@ def main():
         zone_id = r53zone['Id'].replace('/hostedzone/', '')
         zones[r53zone['Name']] = zone_id
 
-    # Verify that the requested zone is already defined in Route53
-    if not zone_in in zones:
-        errmsg = "Zone %s does not exist in Route53" % zone_in
-        module.fail_json(msg = errmsg)
+    if record_in:               # Perform operation on a specific Recordset
+        if not zone_in in zones:
+            errmsg = "Zone %s does not exist in Route53" % zone_in
+            module.fail_json(msg = errmsg)
 
-    record = {}
+        value_list = ()
 
-    found_record = False
-    sets = conn.get_all_rrsets(zones[zone_in])
-    for rset in sets:
-        # Due to a bug in either AWS or Boto, "special" characters are returned as octals, preventing round
-        # tripping of things like * and @.
-        decoded_name = rset.name.replace(r'\052', '*')
-        decoded_name = decoded_name.replace(r'\100', '@')
+        if type(value_in) is str:
+            if value_in:
+                value_list = sorted(value_in.split(','))
+        elif type(value_in)  is list:
+            value_list = sorted(value_in)
 
-        if rset.type == type_in and decoded_name == record_in:
-            found_record = True
-            record['zone'] = zone_in
-            record['type'] = rset.type
-            record['record'] = decoded_name
-            record['ttl'] = rset.ttl
-            record['value'] = ','.join(sorted(rset.resource_records))
-            record['values'] = sorted(rset.resource_records)
-            if value_list == sorted(rset.resource_records) and int(record['ttl']) == ttl_in and state_in == 'present':
+        if record_in[-1:] != '.':
+            record_in += "."
+        if not type_in:
+            module.fail_json(msg = "parameter 'type' required when 'record' is provided")
+        if (state_in == 'present' or state_in == 'absent') and not value_in:
+            module.fail_json(msg = "parameter 'value' required for present/absent when 'record' is provided")
+
+        record = {}
+
+        found_record = False
+        sets = conn.get_all_rrsets(zones[zone_in])
+        for rset in sets:
+            # Due to a bug in either AWS or Boto, "special" characters are returned as octals, preventing round
+            # tripping of things like * and @.
+            decoded_name = rset.name.replace(r'\052', '*')
+            decoded_name = decoded_name.replace(r'\100', '@')
+
+            if rset.type == type_in and decoded_name == record_in:
+                found_record = True
+                record['zone'] = zone_in
+                record['type'] = rset.type
+                record['record'] = decoded_name
+                record['ttl'] = rset.ttl
+                record['value'] = ','.join(sorted(rset.resource_records))
+                record['values'] = sorted(rset.resource_records)
+                if value_list == sorted(rset.resource_records) and int(record['ttl']) == ttl_in and state_in == 'present':
+                    module.exit_json(changed=False)
+
+        if state_in == 'list':
+            module.exit_json(changed=False, set=record)
+
+        if state_in == 'absent' and not found_record:
+            module.exit_json(changed=False)
+
+        changes = ResourceRecordSets(conn, zones[zone_in])
+
+        if state_in == 'present' and found_record:
+            if not module.params['overwrite']:
+                module.fail_json(msg = "Record already exists with different value. Set 'overwrite' to replace it")
+            else:
+                change = changes.add_change("DELETE", record_in, type_in, record['ttl'])
+            for v in record['values']:
+                change.add_value(v)
+
+        if state_in == 'present' or state_in == 'absent':
+            command = 'CREATE' if state_in == 'present' else 'DELETE'
+            change = changes.add_change(command, record_in, type_in, ttl_in)
+            for v in value_list:
+                change.add_value(v)
+
+        try:
+            result = commit(changes, retry_interval_in)
+        except boto.route53.exception.DNSServerError, e:
+            txt = e.body.split("<Message>")[1]
+            txt = txt.split("</Message>")[0]
+            module.fail_json(msg = txt)
+
+        module.exit_json(changed=True)
+    else:                       # Perform operation on Hosted Zone
+        if state_in == 'present':
+            if zone_in in zones:
                 module.exit_json(changed=False)
-
-    if state_in == 'list':
-        module.exit_json(changed=False, set=record)
-
-    if state_in == 'absent' and not found_record:
-        module.exit_json(changed=False)
-
-    changes = ResourceRecordSets(conn, zones[zone_in])
-
-    if state_in == 'present' and found_record:
-        if not module.params['overwrite']:
-            module.fail_json(msg = "Record already exists with different value. Set 'overwrite' to replace it")
+            else:
+                if vpc_in:
+                    if region:
+                        vpc_created = conn.create_zone(zone_in, True, vpc_in, region)
+                    else:
+                        module.fail_json(msg = "parameter 'region' required when 'vpc_in' is provided")
+                else:
+                    vpc_created = conn.create_zone(zone_in)
+                module.exit_json(changed=True, id=vpc_created.id)
+        elif state_in == 'absent':
+            if zone_in in zones:
+                zone_id = zones[zone_in]
+                # TODO: do we have to delete all records first?
+                conn.delete_hosted_zone(zone_id)
+                module.exit_json(changed=True)
+            else:
+                module.exit_json(changed=False)
         else:
-            change = changes.add_change("DELETE", record_in, type_in, record['ttl'])
-        for v in record['values']:
-            change.add_value(v)
-
-    if state_in == 'present' or state_in == 'absent':
-        change = changes.add_change(state_in.upper(), record_in, type_in, ttl_in)
-        for v in value_list:
-            change.add_value(v)
-
-    try:
-        result = commit(changes, retry_interval_in)
-    except boto.route53.exception.DNSServerError, e:
-        txt = e.body.split("<Message>")[1]
-        txt = txt.split("</Message>")[0]
-        module.fail_json(msg = txt)
-
-    module.exit_json(changed=True)
+            errmsg = "Zone %s does not exist in Route53" % zone_in
+            module.fail_json(msg=errmsg)
 
 # import module snippets
 from ansible.module_utils.basic import *
